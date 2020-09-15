168bc62598fe48eb472b0eb9c93ee29be3529e3b
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-09-15 13:12:02 -0300

    Inicia Projeto

---
+++ src/main/java/com/aprendigame/apredigameapi/ApredigameapiApplication.java
@@ -0,0 +1,13 @@
+package com.aprendigame.apredigameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApredigameapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApredigameapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aprendigame/apredigameapi/ApredigameapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendigame.apredigameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApredigameapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
