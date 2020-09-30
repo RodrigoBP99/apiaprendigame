00d3bcff9712b35552abe8eeeec75ad9a04d9f56
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-09-29 21:20:46 -0300

    Cria teste para verificar se não existe student registrado com aquela matricula

--- src/main/java/com/aprendigame/apredigameapi/service/impl/StudentServiceImpl.java
+++ src/main/java/com/aprendigame/apredigameapi/service/impl/StudentServiceImpl.java
@@ -1,6 +1,5 @@
 package com.aprendigame.apredigameapi.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aprendigame.apredigameapi.exception.BusinessRuleException;
--- src/test/java/com/aprendigame/apredigameapi/model/repository/StudentRepositoryTest.java
+++ src/test/java/com/aprendigame/apredigameapi/model/repository/StudentRepositoryTest.java
@@ -1,16 +1,18 @@
 package com.aprendigame.apredigameapi.model.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.aprendigame.apredigameapi.model.entity.Student;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 public class StudentRepositoryTest {
 	
 	@Autowired
@@ -30,6 +32,18 @@ public class StudentRepositoryTest {
 		//verificação
 		Assertions.assertThat(result).isTrue();
 		
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenDontExistARegistredStudent() {
+		//cenário
+		repository.deleteAll();
+		
+		//ação
+		boolean result = repository.existsByRegistration("123456");
+		
+		//verificação
+		Assertions.assertThat(result).isFalse();
 	}
 
 }
