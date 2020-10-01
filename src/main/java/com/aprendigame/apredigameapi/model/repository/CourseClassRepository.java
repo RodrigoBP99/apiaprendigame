c7b9acd56c537fb6746dc9bd8817a709f495806f
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-10-01 19:49:05 -0300

    Cria metodo em CourseClassRepository

--- src/main/java/com/aprendigame/apredigameapi/model/repository/CourseClassRepository.java
+++ src/main/java/com/aprendigame/apredigameapi/model/repository/CourseClassRepository.java
@@ -1,9 +1,15 @@
 package com.aprendigame.apredigameapi.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aprendigame.apredigameapi.model.entity.CourseClass;
 
 public interface CourseClassRepository extends JpaRepository<CourseClass, Long>{
+	
+	boolean existsByCode(String code);
+	
+	Optional<CourseClass> findByCode(String code);
 
 }
