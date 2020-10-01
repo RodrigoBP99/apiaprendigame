8dc9b01cbe276329bde9b1d2bcb1fe9e4d99bb79
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-10-01 19:44:18 -0300

    Cria service para Presenc

---
+++ src/main/java/com/aprendigame/apredigameapi/service/PresencService.java
@@ -0,0 +1,9 @@
+package com.aprendigame.apredigameapi.service;
+
+import com.aprendigame.apredigameapi.model.entity.Presenc;
+
+public interface PresencService {
+	
+	Presenc savePresenc(Presenc presenc);
+
+}
