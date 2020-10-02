c360310576f71b948b80561f9e76125a7d2b401a
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-10-01 21:33:06 -0300

    Cria metodos para CourseUnitRepostory

--- src/main/java/com/aprendigame/apredigameapi/model/repository/CoursesUnitRepository.java
+++ src/main/java/com/aprendigame/apredigameapi/model/repository/CoursesUnitRepository.java
@@ -1,9 +1,15 @@
 package com.aprendigame.apredigameapi.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aprendigame.apredigameapi.model.entity.CoursesUnit;
 
 public interface CoursesUnitRepository extends JpaRepository<CoursesUnit, Long>{
+	
+	boolean existsByCode(String code);
+	
+	Optional<CoursesUnit> findByCode(String code);
 
 }
