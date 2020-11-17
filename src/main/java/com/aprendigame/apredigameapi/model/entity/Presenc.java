61cf91cf8abf68a1a4f5bedc0ce8a14828bdce8a
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-11-17 14:39:56 -0300

    Altera tipo do id

--- src/main/java/com/aprendigame/apredigameapi/model/entity/Presenc.java
+++ src/main/java/com/aprendigame/apredigameapi/model/entity/Presenc.java
@@ -29,7 +29,7 @@ public class Presenc implements Serializable {
 	
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY)
-	private String id;
+	private Long id;
 	private String code;
 	@JsonIgnoreProperties({"students", "quizzes", "teacher", "courseUnit"})
 	private CourseClass courseClass;
